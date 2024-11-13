a9ed9deecedb48798a49d96743cb6254e954b07d
Taniadfs <t.dangelofonfria@gmail.com>
2024-11-13 19:37:44 +0100

    Ejercico 3

--- index.js
+++ index.js
@@ -73,3 +73,36 @@ car2.finalPrice = car2.basePrice + globalBasePrice
 
 console.log(car1)
 console.log(car2)
+
+/*EJERCICIO 3
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.*/
+
+let v = 10
+let f = 5
+console.log(v * f)
+
+/*1.2 Divide 10 por 2 y muestra el resultado en un console.*/
+
+let n = 2
+console.log(v / n)
+
+/*1.3 Muestra mediante un console el resto de dividir 15 por 9.*/
+
+let s = 15
+let q = 9
+console.log(s % q)
+
+/*1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.*/
+
+let p = 10
+let j = 5
+let o = 15
+console.log(p + j)
+
+/*1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+
+let c = 10
+let m = 5
+console.log(c * m)
