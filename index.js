1a4f335f1649c75e2b8a8530c2a42e18b4588d40
Taniadfs <t.dangelofonfria@gmail.com>
2024-11-18 12:49:32 +0100

    EJERCICIO 12

--- index.js
+++ index.js
@@ -328,3 +328,39 @@ function averageWord(param) {
 }
 
 console.log(averageWord(mixedElements))
+
+/*EJERCICIO 12
+Valores únicos: Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+pista (cuando eliminamos algo de un array, su longitud se verá afectada)
+*/
+
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+]
+function removeDuplicates(param) {
+  for (let i = 0; i < param.length; i++) {
+    const check1 = param[i]
+
+    for (let j = i + 1; j < param.length; j++) {
+      const check2 = param[j]
+
+      if (check1 === check2) {
+        param.splice(j, 1)
+        j--
+      }
+    }
+  }
+
+  return param
+}
+console.log(removeDuplicates(duplicates))
