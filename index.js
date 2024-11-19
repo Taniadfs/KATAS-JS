cf95d6e34867a50718cb9c46f9f5ae24d3b5dff8
Taniadfs <t.dangelofonfria@gmail.com>
2024-11-19 13:42:34 +0100

    EJERCICIO 13 Y 14

--- index.js
+++ index.js
@@ -364,3 +364,61 @@ function removeDuplicates(param) {
   return param
 }
 console.log(removeDuplicates(duplicates))
+
+/* EJERCICIO 13
+Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:*/
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+]
+function finderName(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(element)
+    console.log(array[i])
+
+    if (element === array[i]) {
+      return `${true}, position:${i}`
+    }
+  }
+  return false
+}
+console.log(finderName(nameFinder, 'Jessica'))
+
+/* EJERCICIO 14
+Contador de repeticiones: Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma. Puedes usar este array para probar tu función:*/
+
+const counterWords = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'upgrade',
+  'code'
+]
+function repeatCounter(array) {
+  const count = {}
+  for (let i = 0; i < array.length; i++) {
+    if (count[array[i]] <= 1) {
+      count[array[i]]++
+    } else {
+      count[array[i]] = 1
+    }
+  }
+  console.log(count)
+}
+
+console.log(repeatCounter(counterWords))
