d66b840fcd4d487f357c4cf5a85dcd915a9a656b
Taniadfs <t.dangelofonfria@gmail.com>
2024-11-22 21:32:38 +0100

    EJERCICIO 10 Y 11

--- index.js
+++ index.js
@@ -533,3 +533,39 @@ for (const toy of toys1) {
 }
 
 console.log(popularToys)
+
+/*EJERCICIO 21
+Usa un bucle y dos condiciones para imprimir por consola el nombre de los usuarios que sean menor de edad precedidos del texto
+"Usuarios menores de edad:" y otro que imprima a los usuarios mayores de edad, precedido del texto "Usuarios mayores de edad:"*/
+
+const users = [
+  { name: 'Tony', years: 43 },
+  { name: 'Peter', years: 18 },
+  { name: 'Natasha', years: 14 },
+  { name: 'Bruce', years: 32 },
+  { name: 'Khamala', years: 16 }
+]
+
+console.log('Usuarios menores de edad:')
+console.log('Usuarios mayores de edad:')
+
+for (i = 0; i < users.length; i++) {
+  if (users[i].years < 18) {
+    console.log('Menor: ' + users[i].name)
+  } else if (users[i].years >= 18) {
+    console.log('Mayor: ' + users[i].name)
+  }
+}
+
+/*EJERCICIO 22 
+Usa un for para remplazar todas las comidas que no sean veganas con las frutas del array de frutas. Recuerda no usar frutas duplicadas. Finalmente, imprime el array resultante*/
+
+const fruits = ['Strawberry', 'Banana', 'Orange', 'Apple']
+const foodSchedule = [
+  { name: 'Heura', isVegan: true },
+  { name: 'Salmon', isVegan: false },
+  { name: 'Tofu', isVegan: true },
+  { name: 'Burger', isVegan: false },
+  { name: 'Rice', isVegan: true },
+  { name: 'Pasta', isVegan: true }
+]
